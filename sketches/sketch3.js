6055863d86d546656a9bb2614f5d1d7ab7e8eb46
Raizel L <rlagun@uw.edu>
2025-10-27 21:21:27 -0700

    Clock 2: Created static candle image

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,14 +1,33 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
+
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(800, 800);
+    p.noStroke();
   };
+
   p.draw = function () {
-    p.background(240, 200, 200);
-    p.fill(180, 60, 60);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. B', p.width / 2, p.height / 2);
+    p.background(30, 30, 40);
+
+    // candle body
+    p.fill(255, 240, 200);
+    p.rect(180, 180, 40, 150, 10);
+
+    // wick
+    p.fill(50);
+    p.rect(198, 160, 4, 20);
+
+    // flame (outer)
+    p.fill(255, 180, 0);
+    p.ellipse(200, 140, 25, 35);
+
+    // flame (inner)
+    p.fill(255, 255, 150);
+    p.ellipse(200, 140, 12, 20);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+
 });
